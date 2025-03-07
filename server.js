e30eae944b85405e9c2050bac2613e5bdc68dc85
Karu T. Donaldson <karu.donladson@snl.nz>
2025-03-07 16:58:20 +1300

    exclude node_modules from gitignore, add listen on all interfaces to server.js

--- server.js
+++ server.js
@@ -32,4 +32,5 @@ app.post("/api/chat", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+//app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`🚀 Server running on port ${PORT} and accessible externally!`));
